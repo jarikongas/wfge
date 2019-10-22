91de8dd32cb5540f4478a632a3acc3ee7f0014af
jarikongas <43338520+jarikongas@users.noreply.github.com>
2019-10-22 18:59:24 +0300

    Added components

---
+++ src/components/Helloworld.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Helloworld extends Component {
+    render() {
+        return (
+            <div>
+                Hello TOBY
+            </div>
+        )
+    }
+
+
+
+}
